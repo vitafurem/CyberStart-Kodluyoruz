b4a487e271b189e83bbe69b9ba6a5ade87a3988b
vitafurem <164752293+vitafurem@users.noreply.github.com>
2024-05-06 00:09:06 +0300

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,16 @@
+import math
+points=[(5,8),(10,15),(3,9),(11,21),(7,20)]
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance
+
+distances = []
+for i in range(len(points)):
+    for j in range(i+1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+print("Minimum Mesafe:", min_distance)
